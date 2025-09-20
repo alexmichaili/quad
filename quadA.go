58e47a9208b9660a949eac830828de3c1bb871c5
alexmichaili <alexandrousgames@gmail.com>
2025-09-20 17:51:05 +0300

    Update quadA.go

--- quadA.go
+++ quadA.go
@@ -1,19 +1,15 @@
-package main
+// quadA.go
+package quad
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "strings"
 
 func QuadA(x, y int) string {
 	if x <= 0 || y <= 0 {
 		return ""
 	}
 	var builder strings.Builder
-	for i := 1; i <= y; i++ { // rows
-		for j := 1; j <= x; j++ { // columns
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
 			if (i == 1 || i == y) && (j == 1 || j == x) {
 				builder.WriteRune('o')
 			} else if i == 1 || i == y {
@@ -27,18 +23,4 @@ func QuadA(x, y int) string {
 		builder.WriteRune('\n')
 	}
 	return builder.String()
-}
-
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: ./quadA width height")
-		return
-	}
-	x, err1 := strconv.Atoi(os.Args[1])
-	y, err2 := strconv.Atoi(os.Args[2])
-	if err1 != nil || err2 != nil {
-		fmt.Println("Width and height must be integers")
-		return
-	}
-	fmt.Print(QuadA(x, y))
 }
