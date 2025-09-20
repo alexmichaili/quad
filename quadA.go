268353e1ced10c51cdaeb3c061ec46149eee3c6f
alexmichaili <alexandrousgames@gmail.com>
2025-09-20 17:48:06 +0300

    Update quadA.go

--- quadA.go
+++ quadA.go
@@ -1,16 +1,37 @@
-package quad
+package main
 
 import (
 	"fmt"
 	"os"
 	"strconv"
-
-	"github.com/01-edu/z01"
+	"strings"
 )
+
+func QuadA(x, y int) string {
+	if x <= 0 || y <= 0 {
+		return ""
+	}
+	var builder strings.Builder
+	for i := 1; i <= y; i++ { // rows
+		for j := 1; j <= x; j++ { // columns
+			if (i == 1 || i == y) && (j == 1 || j == x) {
+				builder.WriteRune('o')
+			} else if i == 1 || i == y {
+				builder.WriteRune('-')
+			} else if j == 1 || j == x {
+				builder.WriteRune('|')
+			} else {
+				builder.WriteRune(' ')
+			}
+		}
+		builder.WriteRune('\n')
+	}
+	return builder.String()
+}
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Usage: ./program width height")
+		fmt.Println("Usage: ./quadA width height")
 		return
 	}
 	x, err1 := strconv.Atoi(os.Args[1])
@@ -19,25 +40,5 @@ func main() {
 		fmt.Println("Width and height must be integers")
 		return
 	}
-	if x <= 0 || y <= 0 {
-		return
-	}
-	for i := 1; i <= x; i++ { // i rows and j columns
-		for j := 1; j <= y; j++ {
-			if i == 1 || i == x {
-				if j == 1 || j == y {
-					z01.PrintRune('o')
-				} else {
-					z01.PrintRune('-')
-				}
-			} else {
-				if j == 1 || j == y {
-					z01.PrintRune('|')
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-		}
-		z01.PrintRune('\n')
-	}
+	fmt.Print(QuadA(x, y))
 }
